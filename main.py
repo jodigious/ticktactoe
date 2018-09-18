a451561ee7af166f4de51dac46a5fa6b168550c7
josh <joshgalvin12@gmail.com>
2018-09-17 23:21:05 -0700

    Made changes to some of the win_check() logic as well as refined the main execution.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import random
 
 def display_board(board):
 
+    #print("*** func: display_board(board) *** ")
+
     print("\n " + board[7] + "  |  " + board[8] + "  |  " + board[9] + " ")
     print("-" * 15)
     print(" " + board[4] + "  |  " + board[5] + "  |  " + board[6] + " ")
@@ -15,6 +17,8 @@ def display_board(board):
     print(" " + board[1] + "  |  " + board[2] + "  |  " + board[3] + " ")
 
 def player_input():
+
+    #print("*** func: player_input() *** ")
 
     input_correct = None
     player1 = ''
@@ -35,7 +39,7 @@ def player_input():
 
 def place_marker(board, marker, position):
 
-    position = int(input('Please enter a number: '))
+    #print("*** func: place_marker(board, marker, position) *** ")
 
     if position < 1 or position > 9:
         print("\nERROR: Please position in 1-9. ")
@@ -44,6 +48,8 @@ def place_marker(board, marker, position):
 
 def win_check(board, mark):
 
+    #print("*** func: win_check(board, mark) *** ")
+
     gameover = False
 
     if board[5] == mark:
@@ -51,61 +57,70 @@ def win_check(board, mark):
         # Middle Row
         if board[4] == mark and board[6] == mark:
             print("\nGame over!")
+            print("Middle Row for {}".format(mark))
             gameover = True
 
         # Middle Column
         elif board[8] == mark and board[2] == mark:
             print("\nGame over!")
+            print("Middle Column for {}".format(mark))
             gameover = True
 
         # Bottom Left to Top Right
         elif board[1] == mark and board[9] == mark:
             print("\nGame over!")
+            print("Bot left to top right for {}".format(mark))
             gameover = True
 
         # Bottom Right to Top Left
-        elif board[3] == mark and board[7]:
+        elif board[3] == mark and board[7] == mark:
             print("\nGame over!")
+            print("Bot right to top left {}".format(mark))
             gameover = True
 
     # Top Row
     elif board[7] == mark and board [8] == mark and board[9] == mark:
         print("\nGame over!")
+        print("Top row for {}".format(mark))
         gameover = True
 
-    # Borrom Row
+    # Bottom Row
     elif board[1] == mark and board[2] == mark and board[3] == mark:
         print("\nGame over!")
+        print("Bottom Row for {}".format(mark))
         gameover = True
 
     # Left Column
     elif board[1] == mark and board[4] == mark and board[7] == mark:
         print("\nGame over!")
+        print("Left column for {}".format(mark))
         gameover = True
 
     # Right Column
     elif board[3] == mark and board[6] == mark and board[9] == mark:
         print("\nGame over!")
+        print("Right column for {}".format(mark))
         gameover = True
     else:
-        print("\nLet's keep playing!")
+        pass
 
     return gameover
 
 def choose_first():
-    return (random.randrange(1,2))%2 == 0
+    #print("*** func: choose_first() *** ")
+    return (random.randrange(1,100))%2 == 0
 
 def space_check(board, position):
+    #print("*** func: space_check(board, position) *** ")
     return board[position] == ' '
 
 def full_board_check(board):
-    for i in board:
-        if i != ' ':
-            return True
-        else:
-            return False
+    #print("*** func: full_board_check(board) *** ")
+    return not ' ' in board
 
 def player_choice(board):
+
+    #print("*** func: player_choice(board) *** ")
 
     input_correct = None
 
@@ -120,11 +135,13 @@ def player_choice(board):
 
 def replay():
 
+    #print("*** func: replay() *** ")
+
     new_game = None
 
-    x = None
+    redo = None
 
-    while x != True:
+    while redo != True:
 
         resp = input("\nDo you want to play again? [Y/N] : ")
 
@@ -133,10 +150,10 @@ def replay():
 
         elif resp[0].upper() == "Y":
             new_game = True
-            x = True
+            redo = True
         else:
             new_game = False
-            x = True
+            redo = True
     return new_game
 
 
@@ -144,66 +161,74 @@ def replay():
 
 print('\nWelcome to Tic Tac Toe!')
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',]
-player1 = player_input()
-player2 = ''
+player1_mark = player_input()
+player2_mark = ''
 
-if player1.upper() == "X":
-    player2 = 'O'
+if player1_mark.upper() == "X":
+    player2_mark = 'O'
 else:
-    player2 = 'X'
+    player2_mark = 'X'
 
 
 while True:
 
     # Set the game up here
     player1_turn = None
+    game_on = True
+    tie = None
 
     if choose_first() == True:
         player1_turn = True
     else:
         player1_turn = False
 
+    display_board(board)
+
     while game_on:
 
-        display_board(board)
+        tie = None
 
         # Player 1 Turn
         if player1_turn == True:
-            mark = player_input()
-            position = int(input('Please enter a number: '))
+            position = int(input('\nPLAYER 1, Please enter a number: '))
 
             if position < 1 or position > 9:
                 print("\nERROR: Please position in 1-9. ")
-            elif space_check(board, position) == False:
-                print("\nERROR: That space isn't free. ")
             else:
-                place_marker(board, mark, position)
-                display_board(board)
-                if win_check(board, mark) == True:
-                    game_on = False
+                if space_check(board, position) == False:
+                    print("\nERROR: That space isn't free. ")
                 else:
-                    player1_turn = False
+                    place_marker(board, player1_mark, position)
+                    display_board(board)
+                    if win_check(board, player1_mark) == True:
+                        game_on = False
+                        tie = False
+                    else:
+                        player1_turn = False
 
         # Player2's turn.
         else:
-            mark = player_input()
-            position = int(input('Please enter a number: '))
+            position = int(input('\nPLAYER 2, Please enter a number: '))
 
             if position < 1 or position > 9:
                 print("\nERROR: Please position in 1-9. ")
-            elif space_check(board, position) == False:
-                print("\nERROR: That space isn't free. ")
             else:
-                place_marker(board, mark, position)
-                display_board(board)
-
-                if win_check(board, mark) == True:
-                    game_on = False
+                if space_check(board, position) == False:
+                    print("\nERROR: That space isn't free. ")
                 else:
-                    player1_turn = False
+                    place_marker(board, player2_mark, position)
+                    display_board(board)
+                    if win_check(board, player2_mark) == True:
+                        game_on = False
+                        tie = False
+                    else:
+                        player1_turn = True
 
-        if full_board_check(board) == True:
+        if full_board_check(board) and tie != False:
             print("\nLooks like a tie... nicely done. ")
+            break
 
     if not replay():
         break
+    else:
+        board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ]
