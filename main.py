1d56c952e7eaf23efbc877543ed878b3ff1e1add
josh <joshgalvin12@gmail.com>
2018-09-17 21:50:54 -0700

    First attempt at defining all functions. Next commit will include the main.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # Tick Tac Toe game for first milestone project in Python Bootcamp!
 #
 
+import random
+
+
+
 def display_board(board):
 
     if len(board) == 10:
@@ -13,11 +17,15 @@ def display_board(board):
     else:
         print("\nERROR: Please input appropriate board parameters.")
 
+
+
 def player_input():
 
     input_correct = None
+    player1 = ''
 
     while input_correct != True:
+
         player1 = input("\nPlease pick a marker 'X' or 'O': ")
 
         if player1.upper() != 'X' and player1.upper() != 'O':
@@ -27,6 +35,10 @@ def player_input():
         else:
             player1 = player1.upper()
             input_correct = True
+
+    return player1
+
+
 
 def place_marker(board, marker, position):
 
@@ -38,30 +50,124 @@ def place_marker(board, marker, position):
         board[position] = marker
 
 
+
 def win_check(board, mark):
-    pass
+    if board[5] == mark:
+
+        # Middle Row
+        if board[4] == mark and board[6] == mark:
+            print("\nGame over!")
+
+        # Middle Column
+        elif board[8] == mark and board[2] == mark:
+            print("\nGame over!")
+
+        # Bottom Left to Top Right
+        elif board[1] == mark and board[9] == mark:
+            print("\nGame over!")
+
+        # Bottom Right to Top Left
+        elif board[3] == mark and board[7]:
+            print("\nGame over!")
+
+    # Top Row
+    elif board[7] == mark and board [8] == mark and board[9] == mark:
+        print("\nGame over!")
+
+    # Borrom Row
+    elif board[1] == mark and board[2] == mark and board[3] == mark:
+        print("\nGame over!")
+
+    # Left Column
+    elif board[1] == mark and board[4] == mark and board[7] == mark:
+        print("\nGame over!")
+
+    # Right Column
+    elif board[3] == mark and board[6] == mark and board[9] == mark:
+        print("\nGame over!")
+    else:
+        print("\nLet's keep playing!")
+
+
 
 def choose_first():
-    pass
+    return (random.randrange(1,2))%2 == 0
+
+
 
 def space_check(board, position):
-    pass
+    return board[position] == ' '
+
+
 
 def full_board_check(board):
-    pass
+    for i in board:
+        if i != ' ':
+            return True
+        else:
+            return False
+
+
 
 def player_choice(board):
-    pass
+
+    input_correct = None
+
+    while input_correct != True:
+
+        next_position = input("\nWhat's your next choice?: ")
+
+        if position < 1 or position > 9:
+            print("\nERROR: Please position in 1-9. ")
+        else:
+            return space_check(board, next_position)
+
+
 
 def reply():
-    pass
+
+    new_game = None
+
+    x = None
+
+    while x != True
+
+        resp = input("\nDo you want to play again? [Y/N] : ")
+
+        if len(resp) != 1:
+            print("\nPlease respond with 'Y' or 'N")
+
+        elif resp[0].upper() == "Y":
+            new_game = True
+            x = True
+        else:
+            new_game = False
+            x = True
+    return new_game
+
 
 
 
 
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ',]
-display_board(board)
+
+# Main Program starts here.
+print('\nWelcome to Tic Tac Toe!')
+
+while True:
+
+    # Set the game up here
+    pass
+
+    while game_on:
+
+        # Player 1 Turn
 
 
+        # Player2's turn.
 
-display_board(board)
+            pass
+
+    if not replay():
+        break
+# break
